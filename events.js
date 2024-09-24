5904b651d03d83d4d221238c692e9c1efa4d7f32
Quant <174175249+QuantQi@users.noreply.github.com>
2024-09-24 22:37:19 +0100

    divided the page in 2 portions

--- events.js
+++ events.js
@@ -3,6 +3,11 @@
 // Event Handlers
 
 function handleKeyPress(event) {
+    // Ignore if 'Shift' key is pressed
+    if (event.key === 'Shift') {
+        return;
+    }
+
     if (finalResults.classList.contains('hidden')) {
         const typedChar = event.key;
         totalCharsTyped++;
