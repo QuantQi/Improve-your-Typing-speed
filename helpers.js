6feb5929ee019b3631d21ca7ff6bf653be33e2aa
Soubhik <soubhiks@hotmail.com>
2024-09-25 11:36:20 +0100

    simplified updateIncorrectLettersPane

--- helpers.js
+++ helpers.js
@@ -28,7 +28,10 @@ function flashRed(element) {
 }
 
 function updateIncorrectLettersPane() {
-    incorrectLettersElement.innerText = incorrectChars.map(item => item[1] + ' ➔ ' + item[0]).join(', ');
+    incorrectLettersElement.innerText = incorrectChars.map(function(item) {
+        return item[1] + ' -> ' + item[0];
+    }).join(', ');
+    
 
     console.log(`Updated Incorrect Letters: ${incorrectLettersElement.innerText}`);
 }
